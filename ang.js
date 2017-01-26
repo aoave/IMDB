ec54e40e258e35b73287c7b3fb87e74f1427ad89
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2017-01-26 13:45:26 -0500

    Search Funcitonality

--- ang.js
+++ ang.js
@@ -1,7 +1,7 @@
-var myApp = angular.module('myController', []);
-  myApp.controller('', function($scope){
-    $http.get('http://www.omdbapi.com/?i=tt0092455&season=4&ref_=tt_eps_sn_4')
+var myApp = angular.module('myApp', []);
+  myApp.controller('myController', function($scope, $http){
+    $http.get("http://www.omdbapi.com/?i=tt0092455&season=4&ref_=tt_eps_sn_4")
     .success(function(response) {
-      $scope.names = response.title;
+      $scope.episodes = response.title;
     });
   });
