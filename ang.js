1344cbbf41ddd798581a9a6de3e6200f7562bbba
Daniel Cox <danielpcox@gmail.com>
2017-01-26 18:02:06 -0500

    fixing api call and display

--- ang.js
+++ ang.js
@@ -1,7 +1,8 @@
 var myApp = angular.module('myApp', []);
   myApp.controller('myController', function($scope, $http){
-    $http.get("http://www.omdbapi.com/?i=tt0092455&season=4&ref_=tt_eps_sn_4")
-    .then(function(response)
-      {$scope.title = response.episodes;
+    $http.get('http://www.omdbapi.com/?i=tt0092455&season=4&ref_=tt_eps_sn_4')
+    .then(function(response) {
+      console.log(response.data.Episodes)
+      $scope.episodes = response.data.Episodes;
     });
   });
