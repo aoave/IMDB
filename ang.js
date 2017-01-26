6cdc410f051f0fcd607a5417eea881cf4d283a60
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2017-01-26 17:04:35 -0500

    updated js

--- ang.js
+++ ang.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', []);
   myApp.controller('myController', function($scope, $http){
     $http.get("http://www.omdbapi.com/?i=tt0092455&season=4&ref_=tt_eps_sn_4")
-    .success(function(response) {
-      $scope.episodes = response.title;
+    .then(function(response)
+      {$scope.title = response.episodes;
     });
   });
