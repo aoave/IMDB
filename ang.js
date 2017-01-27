ae9ac2672f5ed70d256291e1baea7b8df9fe6df5
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2017-01-26 20:41:09 -0500

    sorting added

--- ang.js
+++ ang.js
@@ -1,7 +1,16 @@
 var myApp = angular.module('myApp', []);
+
   myApp.controller('myController', function($scope, $http){
     $http.get("http://www.omdbapi.com/?i=tt0092455&season=4&ref_=tt_eps_sn_4")
     .then(function(response)
-      {$scope.title = response.episodes;
+
+      {$scope.episodes = response.data.Episodes;
+      $scope.propertyName = 'Released';
+      $scope.reverse = 'true';
+
+      $scope.sortBy =function (propertyName) {
+        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+           $scope.propertyName = propertyName;
+      }
     });
   });
